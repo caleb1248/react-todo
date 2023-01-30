b82495f70aa1eb83298d793ad98ef7a8fc6de565
Caleb Liu <94559723+caleb1248@users.noreply.github.com>
2023-01-30 22:17:55 +0000

    add stuff

--- src/lib/todo.jsx
+++ src/lib/todo.jsx
@@ -1,14 +1,23 @@
-import { Box } from "@mui/material";
-
+import { Box, IconButton, TextField } from "@mui/material";
+import Delete from '@mui/icons-material/Delete';
+import { useState } from 'react';
 export default function Todo({todo}) {
   return (
     <Box
       sx={{
         backgroundColor: "gray",
+				padding: "1rem",
+				borderRadius: "10px"
       }}
       mt={"10px"}
     >
-      {todo.name}
+			<TextField value={todo.name} sx={{
+			backgroundColor: "white",
+			"&:hover": {
+				backgroundColor: "black"
+			}
+			}}></TextField>
+			<IconButton sx={{float: "right"}}><Delete /></IconButton>
     </Box>
   );
 }
