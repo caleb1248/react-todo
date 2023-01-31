003f479956f16b7b946b5720e28cc88a9b24d684
caleb1248 <94559723+caleb1248@users.noreply.github.com>
2023-01-31 07:56:36 -0500

    make input good

--- src/lib/todo.jsx
+++ src/lib/todo.jsx
@@ -1,23 +1,43 @@
 import { Box, IconButton, TextField } from "@mui/material";
-import Delete from '@mui/icons-material/Delete';
-import { useState } from 'react';
-export default function Todo({todo}) {
+import Delete from "@mui/icons-material/Delete";
+import { useState } from "react";
+export default function Todo({ todo }) {
+  const [name, setName] = useState(todo.name);
+
   return (
-    <Box
-      sx={{
-        backgroundColor: "gray",
-				padding: "1rem",
-				borderRadius: "10px"
-      }}
-      mt={"10px"}
-    >
-			<TextField value={todo.name} sx={{
-			backgroundColor: "white",
-			"&:hover": {
-				backgroundColor: "black"
-			}
-			}}></TextField>
-			<IconButton sx={{float: "right"}}><Delete /></IconButton>
+    <Box pt="10px">
+      <Box
+        sx={{
+          backgroundColor: "gray",
+          padding: "1rem",
+          borderRadius: "10px",
+        }}
+      >
+        <TextField
+          value={name}
+          sx={{
+            "input:not(:focus) ~ fieldset": {
+              border: "none",
+            },
+
+            input: {
+              "&:not(:focus)": { cursor: "pointer" },
+              "&:focus": { backgroundColor: "#707070" },
+              caretColor: "white",
+            },
+          }}
+          onChange={({ target }) => {
+            todo.onNameChange(target.value);
+            setName(target.value);
+          }}
+          onKeyUp={({ key, target }) => {
+            if (key == "Enter" || key == "Escape") target.blur();
+          }}
+        />
+        <IconButton sx={{ float: "right" }}>
+          <Delete />
+        </IconButton>
+      </Box>
     </Box>
   );
 }
